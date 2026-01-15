275b3b6aa92bec5d89841bcab1166cb30be5f97d
fleo-fls <jbmuller73@gmail.com>
2026-01-15 10:50:32 -0400

    Phase 12 : ORM (formulaire pour modifier une visite)

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Form\VisiteType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class AdminVoyagesController extends AbstractController
 {
@@ -40,5 +43,30 @@ public function suppr(int $id): Response
 
     return $this->redirectToRoute('admin.voyages');
 }
+
+#[Route('/admin/edit/{id}', name: 'admin.voyage.edit')]
+public function edit(int $id, Request $request): Response
+{
+    $visite = $this->repository->find($id);
+
+    if (!$visite) {
+        throw $this->createNotFoundException('Visite introuvable');
+    }
+
+    $formVisite = $this->createForm(VisiteType::class, $visite);
+    $formVisite->handleRequest($request);
+
+    if ($formVisite->isSubmitted() && $formVisite->isValid()) {
+        $this->repository->add($visite); // ← flush en BDD
+        return $this->redirectToRoute('admin.voyages');
+    }
+
+    return $this->render('admin/admin.voyage.edit.html.twig', [
+        'visite' => $visite,
+        'formvisite' => $formVisite->createView(),
+    ]);
+}
+
+
 }
 
